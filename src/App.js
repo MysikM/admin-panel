dcc09e87ebd08201d13799d106b1016954b72f52
Mysik <Mishamysik@gmail.com>
2022-04-30 11:48:10 +0300

    sidebar create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css'
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
        <div className="App__glass">
-
+            <Sidebar />
+           <div></div>
+           <div></div>
        </div>
     </div>
   );
