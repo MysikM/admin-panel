390571044a8a79a92fd1c96f3c2c7b9e1e63fd1d
Mysik <Mishamysik@gmail.com>
2022-04-30 15:15:27 +0300

    updates block create

---
+++ src/components/Updates/Updates.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './updates.css';
+import {updatesData} from "../../data/Data";
+
+const Updates = () => {
+    return (
+        <div className='updates'>
+            {updatesData.map((update, i) => (
+                <div className='update'>
+                    <img src={update.img} alt={update.name} />
+                    <div className="noti">
+                        <div style={{marginBottom: '0.5rem'}}>
+                            <span>{update.name}</span>
+                            <span> {update.noti}</span>
+                        </div>
+                        <span>{update.time}</span>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Updates;
